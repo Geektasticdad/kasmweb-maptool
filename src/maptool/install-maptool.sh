d4a42a061129fb6b829e459c8c704f0c4dd12cd2
Geektasticdad <73676044+Geektasticdad@users.noreply.github.com>
2022-10-30 11:10:20 -0700

    Building Dockerfile

--- src/maptool/install-maptool.sh
+++ src/maptool/install-maptool.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+set -ex
 wget -O /tmp/maptool_1.12.2-amd64.deb  https://github.com/RPTools/maptool/releases/download/1.12.2/maptool_1.12.2-amd64.deb
 apt install maptool_1.12.2-amd64.deb -y
