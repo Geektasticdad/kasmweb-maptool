d7a923825eed9bf5b02a27f0dc4509e0969ef2a4
Geektasticdad <jason@geektasticdad.com>
2023-05-10 20:54:22 -0700

    Fixes (hopefully)

--- src/maptool/install-maptool.sh
+++ src/maptool/install-maptool.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex
+export PATH=$PATH:/dockerstartup/xdg-utils/scripts
 wget https://github.com/RPTools/maptool/releases/download/1.13.0/maptool_1.13.0-amd64.deb
 apt-get install -y ./maptool_1.13.0-amd64.deb
 chmod +x /opt/maptool/maptool-Maptool.desktop 
