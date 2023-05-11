5312d8c052b21a29b371bbc9a9d9227f763d262f
Geektasticdad <73676044+Geektasticdad@users.noreply.github.com>
2023-05-11 05:50:05 -0700

    Update install-maptool.sh

--- src/maptool/install-maptool.sh
+++ src/maptool/install-maptool.sh
@@ -4,4 +4,4 @@ apt update
 apt-get install -y xdg-utils
 wget https://github.com/RPTools/maptool/releases/download/1.13.0/maptool_1.13.0-amd64.deb
 apt install -y ./maptool_1.13.0-amd64.deb
-#chmod +x /opt/maptool/maptool-Maptool.desktop 
+chmod +x /opt/maptool/lib/maptool-MapTool.desktop 
